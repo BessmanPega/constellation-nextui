0accae011d22d7d24ee097b288bf10d3f148395c
Peter Bessman <peter.bessman@pega.com>
2024-12-17 18:19:21 -0500

    Now the cancel button works. "Works."

--- src/pages/pega.tsx
+++ src/pages/pega.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-console */
 import { useEffect, useMemo, useState } from 'react';
 import { loginIfNecessary, sdkIsLoggedIn } from '@pega/auth/lib/sdk-auth-manager';
@@ -5,10 +6,11 @@ import { getSdkComponentMap } from '@pega/react-sdk-components/lib/bridge/helper
 import { compareSdkPCoreVersions } from '@pega/react-sdk-components/lib/components/helpers/versionHelpers';
 import StoreContext from '@pega/react-sdk-components/lib/bridge/Context/StoreContext';
 import createPConnectComponent from '@pega/react-sdk-components/lib/bridge/react_pconnect';
-import { Button } from "@nextui-org/button";
+import { Button, Card, CardBody, CardFooter, CardHeader } from "@nextui-org/react";
 
 import localSdkComponentMap from '../../sdk-local-component-map';
 
+import { title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
 
 declare const myLoadMashup: any;
@@ -36,9 +38,36 @@ export default function HelloPega() {
   const [rootProps, setRootProps] = useState({});
   const [caseCreated, setCaseCreated] = useState(false);
 
+  /* My naive attempts to make the cancel button work nicely failed, and I've run out of brain
+  juice after getting the "click-away-and-back-again" to work (kludge though it may be). Hence this
+  hack: you click cancel, and the page refreshes. I'm sure there's a nicer way to do this. */
+  const [cancelled, setCancelled] = useState(false);
+  
   useEffect(() => {
     initialize();
-  }, []);
+  }, [isLoggedIn]);
+
+  useEffect(() => {
+    if (caseCreated) {
+      // Subscribe to the EVENT_CANCEL event to handle the assignment cancellation
+      PCore.getPubSubUtils().subscribe(PCore.getConstants().PUB_SUB_EVENTS.EVENT_CANCEL, () => cancelAssignment(), 'cancelAssignment');
+
+      return () => {
+        // unsubscribe to the events
+        PCore.getPubSubUtils().unsubscribe(PCore.getConstants().PUB_SUB_EVENTS.EVENT_CANCEL, 'cancelAssignment');
+      };
+    }
+  }, [caseCreated]);
+
+  const cancelAssignment = () => {
+    setCancelled(true);
+  };
+
+  useEffect(() => {
+    if (cancelled) {
+      window.location.reload();
+    }
+  }, [cancelled])
 
   const initialize = async () => {
     try {
@@ -107,16 +136,33 @@ export default function HelloPega() {
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center">
-        <div className="inline-block text-center justify-center">
-          {isLoggedIn? (
-            caseCreated? ( null ) : (<Button onClick={createCaseButtonClicked}>Create case</Button>)
-          ) : (
-            <h3>Logging in...</h3>
-          )}
-          {caseCreated && (
-            <RootComponent {...rootProps} />
-          )}
-        </div>
+        {cancelled ? (
+          <h3>Cancelling...</h3>
+        ) : (
+          <div className="inline-block text-center justify-center">
+            {!isLoggedIn ? (
+              <h3>Logging in...</h3>
+            ) : (
+              !caseCreated && (
+                <div className="max-w-prose">
+                  <Card className="p-4">
+                    <CardHeader><h1 className={title()}>Welcome!</h1></CardHeader>
+                    <CardBody>
+                      <p>
+                        We're so happy you're interested in our service! We aim to provide the best in the business.
+                        When you're ready to join us, click the button below.
+                      </p>
+                    </CardBody>
+                    <CardFooter>
+                      <Button color='primary' onClick={createCaseButtonClicked}>Let's get started!</Button>
+                    </CardFooter>
+                  </Card>
+                </div>
+              )
+            )}
+            {caseCreated && <RootComponent {...rootProps} />}
+          </div>
+        )}
       </section>
     </DefaultLayout>
   );
